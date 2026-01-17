f4a8a43e4c28d2fd5e8aa0e61c155e0456ed985b
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:05:51 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,50 +4,61 @@
 
 int main()
 {
-int i,j,n,matris[99][99],sum,k,solkosegen[99],sagkosegen[99];
+    int i, j, n, matris[99][99], sum, k, solkosegen[99], sagkosegen[99];
 
     srand(time(NULL));
 
     printf("n i giriniz: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    sum=2;
+    sum = 2;
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
 
-            matris[i][j]=rand()%11;
-
+            matris[i][j] = rand() % 11;
         }
     }
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(i==j || i+j==n-1){
-                printf("%4d",matris[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i == j || i + j == n - 1)
+            {
+                printf("%4d", matris[i][j]);
             }
-            else{
+            else
+            {
                 printf("    ");
             }
         }
         printf("\n");
     }
 
-    for(i=0;i<n;i++){
-        solkosegen[i]=matris[i][i];
-        sagkosegen[i]=matris[i][n-1-i];
+    for (i = 0; i < n; i++)
+    {
+        solkosegen[i] = matris[i][i];
+        sagkosegen[i] = matris[i][n - 1 - i];
     }
-    for(i=0;i<n;i++){
-        matris[i][i]=sagkosegen[n-1-i];
-        matris[i][n-1-i]=solkosegen[n-1-i];
+    for (i = 0; i < n; i++)
+    {
+        matris[i][i] = sagkosegen[n - 1 - i];
+        matris[i][n - 1 - i] = solkosegen[n - 1 - i];
     }
     printf("\n");
 
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            if(i==j || i+j==n-1){
-                printf("%4d",matris[i][j]);
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            if (i == j || i + j == n - 1)
+            {
+                printf("%4d", matris[i][j]);
             }
-            else{
+            else
+            {
                 printf("    ");
             }
         }
